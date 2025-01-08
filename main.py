ddba432a6606f8124960bb1c4ae2e05785aad434
Ezeldeen-Saeed <ezeldeensaeed@gmail.com>
2025-01-08 22:13:03 +0200

    Uploaded trained model

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from teachable import run_object_detection, TeachableModel
 
 confidence_threshold = 0.9
 
-model_path = "./converted_savedmodel2/converted_savedmodel/model.savedmodel/"
-labels_path = "./converted_savedmodel2/converted_savedmodel/labels.txt"
+model_path = "./converted_savedmodel/converted_savedmodel/model.savedmodel/"
+labels_path = "./converted_savedmodel/converted_savedmodel/labels.txt"
 
 
 run_object_detection(model_path, labels_path, confidence_threshold)
